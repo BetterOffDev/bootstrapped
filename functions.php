98a8cd93f6634b7566c47c29d2688ac1ba05da28
wspencer428 <wspencer428@gmail.com>
2014-09-22 20:36:29 -0400

    update README and stub out functions file

--- functions.php
+++ functions.php
@@ -1,2 +1,18 @@
 <?php
 // functions.php
+
+// bring in required core files
+
+// admin area functions
+
+// thumbnail sizes
+
+// sidebars & widgets
+
+// enqueue styles
+
+// enqueue javascript
+
+// comments
+
+// custom post types
